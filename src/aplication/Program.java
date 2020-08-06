efd54e5385787f81a3048be04f4b75d0252f988e
Leandro <cornelio.leandro@gmail.com>
2020-08-06 16:41:06 -0300

    melhoria instancia DAO

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -10,7 +10,7 @@ public class Program {
 	public static void main(String[] args) {
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		Seller sellers = sellerDao.findById(3);
+		Seller sellers = sellerDao.findById(2);
 
 		System.out.println(sellers);
 	}
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -53,15 +53,8 @@ public class SellerDaoJDBC implements SellerDao{
 			st.setInt(1, id);//este id é o id que o metodo retorna como parametro e o 1 é o ?
 			rs = st.executeQuery();
 			if (rs.next()) { //nesse if estamos instanciando os objetos para que seja possivel manipular o mesmo mais a frente
-				Department dep = new Department();
-				dep.setId(rs.getInt("DepartmentId"));
-				dep.setName(rs.getString("DepName"));
-				Seller obj = new Seller();
-				obj.setId(rs.getInt("id"));
-				obj.setName(rs.getString("Name"));
-				obj.setEmail(rs.getString("Email"));
-				obj.setBaseSalary(rs.getDouble("BaseSalary"));
-				obj.setBirthDate(rs.getDate("BirthDate"));
+				Department dep = instantiateDepartment(rs);// metodo auxiliar
+				Seller obj = instantiateSeller(rs, dep);//metodo auxiliar
 				return obj;
 			}
 			return null;
@@ -73,6 +66,24 @@ public class SellerDaoJDBC implements SellerDao{
 			DB.closeResultSet(rs);
 			
 		}
+	}
+
+	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+		Seller obj = new Seller();
+		obj.setId(rs.getInt("Id"));
+		obj.setName(rs.getString("Name"));
+		obj.setEmail(rs.getString("Email"));
+		obj.setBaseSalary(rs.getDouble("BaseSalary"));
+		obj.setBirthDate(rs.getDate("BirthDate"));
+		obj.setDepartment(dep);
+		return obj;
+	}
+
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("DepartmentId"));
+		dep.setName(rs.getString("DepName"));
+		return dep;
 	}
 
 	@Override
