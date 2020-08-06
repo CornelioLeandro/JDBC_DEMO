4d7759e0462fe871572130b1c955d1539db48bd8
Leandro <cornelio.leandro@gmail.com>
2020-08-06 16:33:12 -0300

    FindById

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,23 +1,18 @@
 package aplication;
 
-import java.util.Date;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1,"Hardware");
-		System.out.println(obj);
-		
-		Seller seller = new Seller(23, "Leandro", "leandro@gmail.com", new Date(), 855.09 , obj);
-		System.out.println(seller);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller sellers = sellerDao.findById(3);
+
+		System.out.println(sellers);
 	}
 
 }
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,10 +1,13 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
-
+// esta classe foi criada para que a implementacao no banco não seja exposta no programa princial e resolve o problema de 
+//injecao de dependencia por isso ela chama uma Interface SELLERDAO
+	
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 }
